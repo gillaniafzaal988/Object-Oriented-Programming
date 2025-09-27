aec6612a8cb29b7fbbb88ed946ea8995f3080483
gillaniafzaal988 <gillaniafzaal988@gmail.com>
2025-09-28 00:38:02 +0500

    Update main.cpp

--- LAB 06(Runtime polymorphisms)/main.cpp
+++ LAB 06(Runtime polymorphisms)/main.cpp
@@ -1,14 +1,23 @@
 
-#include "Calculator.h"
-#include "Distance.h"
+#include "Empolyee.h"
+#include "Shape.h"
 
 using namespace std;
 int main(){
 	
 	   
 	//01task
+    Employee* emp1 = new PermanentEmployee(30000, 5000);
+    Employee* emp2 = new ContractEmployee(500, 40);
 
+    cout << "Permanent Employee Salary: " << emp1->calculateSalary() << endl;
+    cout << "Contract Employee Salary: " << emp2->calculateSalary() << endl;
+
+    delete emp1;
+    delete emp2;
+//task 2 
    
       
     return 0;
 }
+
