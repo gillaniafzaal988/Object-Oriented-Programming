3c52370d4509bb10efcc3816005ebcbd6278c68b
gillaniafzaal988 <gillaniafzaal988@gmail.com>
2025-09-27 23:20:18 +0500

    Add files via upload

---
+++ lab 04 (Inheritance)/Person.h
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Person{
+	string name;
+	int age;
+	public:
+		Person(string n, int a){
+			name=n;
+			age=a;
+		}
+		void display_person_info(){
+			cout<<"Name:"<<name;
+			cout<<"\nAge:"<<age;
+		}
+};
+class Student:public Person{
+	int studentid;
+	public:
+		Student(string n, int a, int id):Person(n,a){
+			studentid =id;
+		}
+	  	void display_student_info(){
+	  	display_person_info();
+		cout<<"\nstudent id:"<<studentid;	
+		}
+		
+};
---
+++ lab 04 (Inheritance)/main.cpp
@@ -0,0 +1,11 @@
+#include "Person.h"
+#include "Employee.h"
+int main(){
+	//task01
+	Student s("afzaal",20,888);
+	s.display_student_info();
+	//task02
+	Manager M("software","afzaal",13,666);
+	M.display_manager();
+	return 0;
+}
